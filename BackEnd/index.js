b7cc0dc34d9a4de18e6b17cff379aeb56c41951c
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2024-06-29 12:04:55 +0530

    updated

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -5,17 +5,15 @@ import cookieParser from "cookie-parser";
 // Utiles
 
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5001;
-
 connectDB()
-
 const app = express()
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-app.get("/", (req, res) => {
-    res.send("Hello world");
-});
+app.use("/api/users", userRoutes);
 app.listen(port, () => console.log(`Server running on port: ${port}`));
