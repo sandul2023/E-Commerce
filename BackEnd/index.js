022f7e43d9f6791c3fb8efc057f73a3b990d547e
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2024-06-29 21:47:13 +0530

    order updated

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -19,10 +19,10 @@ connectDB()
 const app = express()
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/api/users", userRoutes);
+app.use("/api/users",userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
